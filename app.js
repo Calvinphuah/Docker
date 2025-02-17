f40316ea243792323155ad388b2ee9bba61982a9
calvinphuah <calvinphuah73@gmail.com>
2025-02-17 23:19:29 +0800

    save

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: "1",
+      title: "Snakes and Ladders",
+    },
+    {
+      id: "2",
+      title: "Monopoly",
+    },
+    {
+      id: "3",
+      title: "Chess",
+    },
+  ]);
+});
+
+app.listen(4000, () => {
+  console.log("listening for requests on port 4000");
+});
